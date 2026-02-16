46d7210eedeeae81c60b9f96d2d6d25d2044a193
Jomar Milan <jomarm@jomarm.com>
2026-02-15 16:07:28 -0800

    Add thunar, okular

--- machines/dell-inspiron7773/modules/user.nix
+++ machines/dell-inspiron7773/modules/user.nix
@@ -1,7 +1,13 @@
-{ config, pkgs, lib, ... }:
+{
+  config,
+  pkgs,
+  lib,
+  ...
+}:
 let
   cfg = config.machine.user;
-in {
+in
+{
   options.machine.user = {
     enable = lib.mkEnableOption "dell-inspiron7773 user module";
     sources = lib.mkOption {
@@ -15,126 +21,142 @@ in {
     users.users.jomarm = {
       isNormalUser = true;
       description = "Jomar Milan";
-      extraGroups = [ "networkmanager" "wheel" "docker" ];
-      packages = with pkgs; [];
+      extraGroups = [
+        "networkmanager"
+        "wheel"
+        "docker"
+      ];
+      packages = with pkgs; [ ];
     };
 
-    home-manager.users.jomarm = { config, pkgs, wrappers, ... }: {
-      imports = [
-        ../../../home-manager/modules
+    home-manager.users.jomarm =
+      {
+        config,
+        pkgs,
+        wrappers,
+        ...
+      }:
+      {
+        imports = [
+          ../../../home-manager/modules
 
-        (import ../../../home-manager/inputs.nix { inherit (cfg) sources; })
-      ];
+          (import ../../../home-manager/inputs.nix { inherit (cfg) sources; })
+        ];
 
-      home.stateVersion = "24.11";
+        home.stateVersion = "24.11";
 
-      usermod.unfree.pkgnames = [
-        "aseprite"
-        "idea"
-        "rider"
-      ];
+        usermod.unfree.pkgnames = [
+          "aseprite"
+          "idea"
+          "rider"
+        ];
 
-      home.packages = with pkgs; [
-        (limo.override { withUnrar = true; })
-        dragon-drop
-        aseprite
-        (buildFHSEnv {
-          # bit of a mess of an adhoc wrapper...
-          name = "stellaris-gog-unpacked-env";
-          includeClosures = true;
-          # Based on steam env
-          targetPkgs = pkgs: with pkgs; [
-            bash
-            coreutils
-            file
-            lsb-release
-            pciutils
-            glibc_multi.bin
-            usbutils
-            xdg-utils
-            xz
-            zenity
-          ];
-          # Based on steam env, + nss and nspr
-          multiPkgs = pkgs: with pkgs; [
-            glibc
-            libxcrypt
-            libGL
-            libdrm
-            libgbm
-            udev
-            libudev0-shim
-            libva
-            vulkan-loader
-            networkmanager
-            libcap
-            nss
-            nspr
-          ];
-          extraInstallCommands = ''
-            mkdir -p $out/share/applications
-            cat > $out/share/applications/gog_com-Stellaris_1.desktop <<EOF
-            [Desktop Entry]
-            Encoding=UTF-8
-            Value=1.0
-            Type=Application
-            Name=Stellaris
-            GenericName=Stellaris
-            Comment=Stellaris
-            Icon=${ config.home.homeDirectory }/GOG Games/Stellaris/support/icon.png
-            Exec="$out/bin/stellaris-gog-unpacked-env" ""
-            Categories=Game;
-            Path=${ config.home.homeDirectory }/GOG Games/Stellaris
-            EOF
-          '';
-          runScript = "\"${ config.home.homeDirectory }\"/GOG\\ Games/Stellaris/start.sh";
-        })
-        jetbrains.idea
-        wrappers.rider
-      ];
+        home.packages = with pkgs; [
+          (limo.override { withUnrar = true; })
+          dragon-drop
+          aseprite
+          jetbrains.idea
+          wrappers.rider
+          kdePackages.okular
+          (buildFHSEnv {
+            # bit of a mess of an adhoc wrapper...
+            name = "stellaris-gog-unpacked-env";
+            includeClosures = true;
+            # Based on steam env
+            targetPkgs =
+              pkgs: with pkgs; [
+                bash
+                coreutils
+                file
+                lsb-release
+                pciutils
+                glibc_multi.bin
+                usbutils
+                xdg-utils
+                xz
+                zenity
+              ];
+            # Based on steam env, + nss and nspr
+            multiPkgs =
+              pkgs: with pkgs; [
+                glibc
+                libxcrypt
+                libGL
+                libdrm
+                libgbm
+                udev
+                libudev0-shim
+                libva
+                vulkan-loader
+                networkmanager
+                libcap
+                nss
+                nspr
+              ];
+            extraInstallCommands = ''
+              mkdir -p $out/share/applications
+              cat > $out/share/applications/gog_com-Stellaris_1.desktop <<EOF
+              [Desktop Entry]
+              Encoding=UTF-8
+              Value=1.0
+              Type=Application
+              Name=Stellaris
+              GenericName=Stellaris
+              Comment=Stellaris
+              Icon=${config.home.homeDirectory}/GOG Games/Stellaris/support/icon.png
+              Exec="$out/bin/stellaris-gog-unpacked-env" ""
+              Categories=Game;
+              Path=${config.home.homeDirectory}/GOG Games/Stellaris
+              EOF
+            '';
+            runScript = "\"${config.home.homeDirectory}\"/GOG\\ Games/Stellaris/start.sh";
+          })
+        ];
 
-      home.shellAliases = {
-        img = "${ pkgs.chafa }/bin/chafa";
-      };
-
-      usermod.email.enable = true;
-      usermod.gpg.enable = true;
-      usermod.git.enable = true;
-      usermod.shell.enable = true;
-      usermod.ssh.enable = true;
-      usermod.aerc.enable = true;
-      usermod.offlineimap.enable = true;
-      usermod.niri.enable = true;
-      usermod.noctalia-shell.enable = true;
-      usermod.stylix.enable = true;
-      usermod.firefox.enable = true;
-      usermod.secrets.enable = true;
-      usermod.term.enable = true;
-      usermod.portty.enable = true;
-      usermod.hydrus.enable = true;
-      usermod.atool.enable = true;
-      usermod.wallpaper.enable = true;
-      usermod.zed-editor.enable = true;
-
-      programs.noctalia-shell.package = pkgs.callPackage "${ cfg.sources.noctalia-shell }/nix/package.nix" {};
-      programs.niri.settings = {
-        debug = {
-          render-drm-device = "/dev/dri/card1";
+        home.shellAliases = {
+          img = "${pkgs.chafa}/bin/chafa";
         };
-      };     
 
-      xdg.portal.enable = true;
+        usermod.email.enable = true;
+        usermod.gpg.enable = true;
+        usermod.git.enable = true;
+        usermod.shell.enable = true;
+        usermod.ssh.enable = true;
+        usermod.aerc.enable = true;
+        usermod.offlineimap.enable = true;
+        usermod.niri.enable = true;
+        usermod.noctalia-shell.enable = true;
+        usermod.stylix.enable = true;
+        usermod.firefox.enable = true;
+        usermod.secrets.enable = true;
+        usermod.term.enable = true;
+        usermod.thunar.enable = true;
+        usermod.hydrus.enable = true;
+        usermod.atool.enable = true;
+        usermod.wallpaper.enable = true;
+        usermod.zed-editor.enable = true;
 
-      xdg.mimeApps = {
-        enable = true;
+        programs.noctalia-shell.package =
+          pkgs.callPackage "${cfg.sources.noctalia-shell}/nix/package.nix"
+            { };
+        programs.niri.settings = {
+          debug = {
+            render-drm-device = "/dev/dri/card1";
+          };
+        };
 
-        defaultApplications = {
-          "text/html" = "firefox.desktop";
-          "x-scheme-handler/http" = "firefox.dekstop";
-          "x-scheme-handler/https" = "firefox.desktop";
-          "application/x-extension-html" = "firefox.desktop";
+        # xdg.portal.enable = true;
+
+        xdg.mimeApps = {
+          enable = true;
+
+          defaultApplications = {
+            "text/html" = "firefox.desktop";
+            "x-scheme-handler/http" = "firefox.dekstop";
+            "x-scheme-handler/https" = "firefox.desktop";
+            "application/x-extension-html" = "firefox.desktop";
+          };
         };
       };
-    };
   };
 }
