4a56491fadcf0e986cf0afe5d57464949e2cf55a
Jomar Milan <jomarm@jomarm.com>
2026-04-26 20:48:08 -0700

    Use sddm-astronaut theme

--- machines/dell-inspiron7773/modules/graphical-session.nix
+++ machines/dell-inspiron7773/modules/graphical-session.nix
@@ -1,7 +1,20 @@
-{ config, lib, ... }:
+{
+  config,
+  pkgs,
+  lib,
+  ...
+}:
 let
   cfg = config.machine.graphical-session;
-in {
+  themepkg = pkgs.sddm-astronaut.override {
+    themeConfig = {
+      BackgroundPlaceholder = "";
+      Background = "${../../../home-manager/home-manager-private}/2533132599-1-screenshot.png";
+    };
+    embeddedTheme = "hyprland_kath";
+  };
+in
+{
   options.machine.graphical-session = {
     enable = lib.mkEnableOption "dell-insprion7773 graphical session config";
   };
@@ -12,6 +25,8 @@ in {
     services.displayManager.sddm = {
       enable = true;
       wayland.enable = true;
+      theme = "${themepkg}/share/sddm/themes/sddm-astronaut-theme";
+      extraPackages = [ themepkg ];
     };
   };
 }
