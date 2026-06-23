9e72874b1ad3eefd92e5a463865757b7235d7b59
Jomar Milan <jomarm@pyrodax.com>
2026-06-23 12:26:40 -0700

    Setup XMP server on raspberrypi

--- machines/raspberrypi/configuration.nix
+++ machines/raspberrypi/configuration.nix
@@ -17,6 +17,7 @@
     isNormalUser = true;
     description = "Jomar Milan";
     extraGroups = [ "wheel" ];
+    initialPassword = "raspberrypi";
     packages = with pkgs; [ ];
     openssh.authorizedKeys.keys = [
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHOfZ3GVOc4/hDqYzxlfg41y5MgtVD9WQxmN90QtHt4I jomarm@Jomars-MacBook-Pro.local"
@@ -30,6 +31,84 @@
       PasswordAuthentication = false;
       PermitRootLogin = "no";
       AllowGroups = [ "users" ];
+    };
+  };
+
+  services.nginx = {
+    enable = true;
+    virtualHosts."pyrodax.com" = {
+      forceSSL = true;
+      useACMEHost = "pyrodax.com";
+      locations."/.well-known/".root = "/var/lib/acme/acme-challenge/";
+    };
+  };
+
+  services.prosody = {
+    enable = true;
+    admins = [ "jomarm@pyrodax.com" ];
+    ssl.cert = "/var/lib/prosody/fullchain.pem";
+    ssl.key = "/var/lib/prosody/key.pem";
+    virtualHosts."pyrodax.com" = {
+      enabled = true;
+      domain = "pyrodax.com";
+      ssl.cert = "/var/lib/prosody/fullchain.pem";
+      ssl.key = "/var/lib/prosody/key.pem";
+    };
+    muc = [ { domain = "conference.pyrodax.com"; } ];
+    httpFileShare = {
+      domain = "upload.pyrodax.com";
+    };
+    extraConfig = ''
+      -- only needed for prosodyctl check dns
+      external_addresses = { "24.130.29.83", "2601:640:cc02:cd3e:6463:1172:452e:9e21" }
+    '';
+  };
+
+  security.acme = {
+    acceptTerms = true;
+    defaults.email = "jomarm@pyrodax.com";
+    defaults.webroot = "/var/lib/acme/acme-challenge/";
+    certs = {
+      "pyrodax.com" = {
+        group = config.services.nginx.group;
+        extraDomainNames = [
+          "conference.pyrodax.com"
+          "upload.pyrodax.com"
+        ];
+        postRun = ''
+          install -o prosody -g prosody fullchain.pem /var/lib/prosody/fullchain.pem
+          install -o prosody -g prosody key.pem /var/lib/prosody/key.pem
+        '';
+      };
+    };
+  };
+
+  networking.firewall.allowedTCPPorts = [
+    # Standard HTTP/HTTPS
+    80
+    443
+
+    # XMPP
+    5000
+    5222
+    5269
+    5281
+  ];
+
+  system.activationScripts = {
+    expire-passwords = {
+      deps = [ "users" ];
+      text = ''
+        if ! ${pkgs.systemd}/bin/systemd-detect-virt --quiet; then
+          if [ ! -e /var/lib/expire-passwords/jomarm ]; then
+            if id -u >/dev/null 2>&1; then
+              ${pkgs.shadow}/bin/chage -d 0 jomarm
+              mkdir -p /var/lib/expire-passwords
+              touch /var/lib/expire-passwords/jomarm
+            fi
+          fi
+        fi
+      '';
     };
   };
 
@@ -63,6 +142,8 @@
       ];
     };
   };
+
+  nix.settings.trusted-users = [ "@wheel" ];
 
   nixpkgs.hostPlatform = "aarch64-linux";
 
