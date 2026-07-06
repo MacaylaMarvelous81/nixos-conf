9127a278a79fc6bf63e9697f55ab533771eed5e0
Jomar Milan <jomarm@pyrodax.com>
2026-07-05 18:24:32 -0700

    Cross-compile continuwuity 0.5.10 for raspberrypi

--- machines/raspberrypi/configuration.nix
+++ machines/raspberrypi/configuration.nix
@@ -4,6 +4,40 @@
   nixos-raspberrypi,
   ...
 }:
+let
+  pkgsHost = import pkgs.path {
+    localSystem = "x86_64-linux";
+  };
+
+  pkgsCross = import pkgs.path {
+    overlays = [
+      (final: prev: {
+        matrix-continuwuity = prev.matrix-continuwuity.overrideAttrs (
+          finalAttrs: prevAttrs: {
+            version = "0.5.10";
+
+            src = final.fetchFromGitea {
+              domain = "forgejo.ellis.link";
+              owner = "continuwuation";
+              repo = "continuwuity";
+              tag = "v${finalAttrs.version}";
+              hash = "sha256-oevEGYlAK/rMJhm200CkwerT5oVak8sJj0Fa6r6+J/Q=";
+            };
+
+            cargoDeps = prev.rustPlatform.fetchCargoVendor {
+              inherit (finalAttrs) version src;
+              name = "${finalAttrs.pname}-${finalAttrs.version}-vendor";
+              hash = "sha256-uvMiFURXxkLbbbwq4pG5hevsLZHQ1wVfTNvzQRTQWxE=";
+            };
+          }
+        );
+      })
+    ];
+
+    localSystem = "x86_64-linux";
+    crossSystem = "aarch64-linux";
+  };
+in
 {
   imports = with nixos-raspberrypi.nixosModules; [
     raspberry-pi-5.base
@@ -234,6 +268,20 @@
   nix.settings.trusted-users = [ "@wheel" ];
 
   nixpkgs.hostPlatform = "aarch64-linux";
+  nixpkgs.overlays = [
+    (final: prev: {
+      matrix-continuwuity = pkgsCross.matrix-continuwuity.override {
+        inherit (final)
+          bzip2
+          zstd
+          rust-jemalloc-sys-unprefixed
+          liburing
+          rocksdb
+          ;
+        inherit (pkgsHost) pkg-config;
+      };
+    })
+  ];
 
   system.stateVersion = "25.11";
 }
