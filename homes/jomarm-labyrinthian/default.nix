0b0d4b1c2ebebfe77b0aa6f9da72fd7b351bcab5
Jomar Milan <jomarm@pyrodax.com>
2026-08-02 18:12:49 -0700

    homes.jomarm-labyrinthian: add dunst config

--- homes/jomarm-labyrinthian/default.nix
+++ homes/jomarm-labyrinthian/default.nix
@@ -215,6 +215,19 @@ in
       };
     };
 
+    services.dunst = {
+      enable = true;
+      settings.global = lib.mkMerge [
+        {
+          mouse_left_click = "context,close_current";
+          mouse_middle_click = "close_all";
+          mouse_right_click = "close_current";
+        }
+        (lib.mkIf config.programs.rofi.enable {
+          dmenu = "${config.programs.rofi.package}/bin/rofi -dmenu -p dunst";
+        })
+      ];
+    };
     services.gpg-agent = {
       enable = true;
       enableBashIntegration = true;
