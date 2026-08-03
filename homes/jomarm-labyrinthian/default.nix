e641da266ed34d978b01921bce17b97d0ed125ba
Jomar Milan <jomarm@pyrodax.com>
2026-08-02 18:07:41 -0700

    homes.jomarm-labyrinthian: add waybar config

--- homes/jomarm-labyrinthian/default.nix
+++ homes/jomarm-labyrinthian/default.nix
@@ -146,6 +146,74 @@ in
       enable = true;
       package = pkgs.qalculate-qt;
     };
+    programs.waybar = {
+      enable = true;
+      systemd.enable = true;
+      settings = {
+        bar = {
+          layer = "top";
+          position = "top";
+          modules-left = [
+            "niri/window"
+            "mpris"
+            "custom/spacer"
+            "wireplumber"
+          ];
+          modules-center = [
+            "clock"
+          ];
+          modules-right = [
+            "tray"
+            "cpu"
+            "custom/spacer"
+            "memory"
+            "custom/spacer"
+            "disk"
+            "custom/spacer"
+            "network"
+            "custom/spacer"
+            "bluetooth"
+          ];
+
+          "niri/window" = {
+            format = "> {title}";
+          };
+          mpris = {
+            format = "<span color=\"#e3b872\">{player_icon}</span> {dynamic}";
+            format-paused = "{status_icon} <i>{dynamic}</i>";
+          };
+          tray = {
+            icon-size = 21;
+            spacing = 10;
+          };
+          cpu = {
+            format = " <span color=\"#e3b872\">cpu</span> {usage}";
+            states = {
+              warning = 80;
+              critical = 95;
+            };
+          };
+          memory = {
+            format = "<span color=\"#e3b872\">mem</span> {used:0.1f}G/{total:0.1f}G";
+          };
+          disk = {
+            format = "<span color=\"#e3b872\">dsk</span> {free}/{total}";
+          };
+          network = {
+            format = "{ifname} ({ipaddr})";
+            format-wifi = "{essid} ({ipaddr})";
+          };
+          bluetooth = {
+            format = "<span color=\"#66c0f4\">bt</span> {status}";
+            on-click = "${pkgs.blueman}/bin/blueman-manager";
+          };
+          "custom/spacer" = {
+            format = " | ";
+            tooltip = false;
+          };
+        };
+      };
+    };
 
     services.gpg-agent = {
       enable = true;
