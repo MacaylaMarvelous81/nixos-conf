0eb247925dea760962b9cb975c04a6232ea15160
Jomar Milan <jomarm@pyrodax.com>
2026-08-09 15:47:00 -0700

    worlds.labyrinthia: use qylock pixel waterfall sddm theme

--- worlds/labyrinthia/graphical-session.nix
+++ worlds/labyrinthia/graphical-session.nix
@@ -6,15 +6,35 @@
 }:
 let
   cfg = config.worlds.labyrinthia.graphical-session;
-  themepkg = pkgs.sddm-astronaut.override {
-    themeConfig = {
-      BackgroundPlaceholder = "";
-      Background = pkgs.fetchurl {
-        url = "https://images.steamusercontent.com/ugc/1701780623175527143/5BCE317B403D24A26A56EA0C42B516662E5C8896/";
-        hash = "sha256-Br0nOQkeLmSfKyrzCqMa1kwsxz2xGm1ILC1hV4wfhEQ=";
-      };
+  # themepkg = pkgs.sddm-astronaut.override {
+  #   themeConfig = {
+  #     BackgroundPlaceholder = "";
+  #     Background = pkgs.fetchurl {
+  #       url = "https://images.steamusercontent.com/ugc/1701780623175527143/5BCE317B403D24A26A56EA0C42B516662E5C8896/";
+  #       hash = "sha256-Br0nOQkeLmSfKyrzCqMa1kwsxz2xGm1ILC1hV4wfhEQ=";
+  #     };
+  #   };
+  #   embeddedTheme = "hyprland_kath";
+  # };
+  themepkg = pkgs.stdenvNoCC.mkDerivation {
+    pname = "qylock-sddm-themes";
+    version = "0-unstable-2026-08-05";
+
+    src = pkgs.fetchFromGitHub {
+      owner = "Darkkal44";
+      repo = "qylock";
+      rev = "519310704a917733d6afeb597f27b3845518da51";
+      hash = "sha256-++Tykq/IJLSWg7ivS3cT3MPEdjay2vWwYJR0tZ2r1NI=";
     };
-    embeddedTheme = "hyprland_kath";
+
+    installPhase = ''
+      runHook preInstall
+
+      mkdir -p $out/share/sddm/themes
+      cp -r themes/. $out/share/sddm/themes/
+
+      runHook postInstall
+    '';
   };
 in
 {
@@ -22,16 +42,19 @@ in
     enable = lib.mkEnableOption "Default graphical sessions for Labyrinthia";
   };
 
-  config = lib.mkIf cfg.enable (
-    lib.mkDefault {
-      programs.niri.enable = true;
+  config = lib.mkIf cfg.enable {
+    programs.niri.enable = true;
 
-      services.displayManager.sddm = {
-        enable = true;
-        wayland.enable = true;
-        theme = "${themepkg}/share/sddm/themes/sddm-astronaut-theme";
-        extraPackages = [ themepkg ];
-      };
-    }
-  );
+    services.displayManager.sddm = {
+      enable = true;
+      wayland.enable = true;
+      theme = "${themepkg}/share/sddm/themes/pixel-waterfall";
+      extraPackages = [
+        themepkg
+        pkgs.qt6.qt5compat
+        pkgs.qt6.qtmultimedia
+        pkgs.qt6.qtsvg
+      ];
+    };
+  };
 }
